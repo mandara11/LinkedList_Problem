b132bcf6af221de32eddd0e998e2b0807e35d777
mandara11 <mandaram2345@gmail.com>
2022-08-16 07:26:54 +0530

    Added:UC1

--- Program.cs
+++ Program.cs
@@ -5,6 +5,11 @@
         public static void Main(string[] args)
         {
             Console.WriteLine("Welcome to LinkedList Program");
+            LinkedList list = new LinkedList();
+            list.Add(56);
+            list.Add(30);
+            list.Add(70);
+            list.Display();
         }
     }
 }
