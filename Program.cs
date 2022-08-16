56085ef32f930fca495275bcd3e9dd8ad0cd6805
mandara11 <mandaram2345@gmail.com>
2022-08-16 08:10:53 +0530

    Added:UC9

--- Program.cs
+++ Program.cs
@@ -38,11 +38,22 @@
             //list.Search(30);
 
             //UC8
+            //list.Add(56);
+            //list.Add(30);
+            //list.Add(70);
+            //int newNode = list.Search(30);
+            //list.InsertAtParticularPosition(newNode + 1, 40);
+            //list.Display();
+
+            //UC9
             list.Add(56);
             list.Add(30);
             list.Add(70);
-            int newNode = list.Search(30);
-            list.InsertAtParticularPosition(newNode + 1, 40);
+            int node = list.Search(30);
+            list.InsertAtParticularPosition(node + 1, 40);
+            list.Display();
+            Console.WriteLine("\n\nElement Delete SuccessFully");
+            list.DeleteNodeAtParticularPosition(node + 1);
             list.Display();
         }
     }
