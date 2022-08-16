202fb792f8d962432cfa655beda509582c45cd1b
mandara11 <mandaram2345@gmail.com>
2022-08-16 07:43:45 +0530

    Added:UC4

--- Program.cs
+++ Program.cs
@@ -6,9 +6,17 @@
         {
             Console.WriteLine("Welcome to LinkedList Program");
             LinkedList list = new LinkedList();
+            //list.Add(56);
+            //list.Add(30);
+            //list.Add(70);
+            //list.Display();
+
+            //UC4
             list.Add(56);
-            list.Add(30);
             list.Add(70);
+            int a = list.Search(56);
+            list.InsertData(a + 1, 30);
+
             list.Display();
         }
     }
